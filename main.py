9df9be53525d760a9a76476e710f0f33c7a2c6c9
josephlin8909 <josephlin89@gmail.com>
2024-10-24 14:00:10 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from sokobanGame import Sokoban as game
+import argparse
+
+# from nnwrapper import NNetWrapper as nn
+# from coach import Coach
+import torch
+import os
+import sys
+
+
+def display_board(board):
+    """
+    Displays the current state of the game board.
+    """
+    print(board)
+
+
+def get_user_move():
+    """
+    Captures user input and maps it to a game move.
+    w - up, s - down, a - left, d - right
+    """
+    move_mapping = {
+        "w": (0, -1),  # up
+        "d": (1, 0),  # right
+        "s": (0, 1),  # down
+        "a": (-1, 0),  # left
+    }
+
+    while True:
+        move = input("Move (w/a/s/d): ").lower()
+        if move in move_mapping:
+            return move_mapping[move]
+        else:
+            print("Invalid input! Use 'w' (up), 'a' (left), 's' (down), 'd' (right).")
+
+
+if __name__ == "__main__":
+    fh = open(os.path.join("data", "puzzle1-1.txt"))
+    fcontent = fh.read()
+    fh.close()
+
+    sys.setrecursionlimit(10000)
+    g = game(fcontent)
+    board = g.get_initial_board()
+
+    while not g.has_puzzle_ended(board):
+        display_board(board)
+        move = get_user_move()
+        board.execute_move(move)
+
+    print("Congratulations! You've solved the puzzle!")
